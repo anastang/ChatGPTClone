fd98391694a83cca2ba8dd6b2fce2ad3e7582d7c
Anastan Gnanapragasam <anastan.gp@gmail.com>
2023-05-11 21:57:38 -0400

    Added commnets + changed role of ai

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,55 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
-import  { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+// Define your OpenAI API key
+const API_KEY = "ADD_KEY_HERE";
+
+// Define a system message for PlayByGPT
+const systemMessage = {
+  "role": "system",
+  "content": "Explain things like you're a professional sports analyst and you are trying to provide accurate information to your client."
+}
 
 function App() {
-  const [typing, setTyping] = useState(false);
+  // Initialize state variables for messages and typing indicator
   const [messages, setMessages] = useState([
     {
-      message: "Hello, I am ChatGPT!",
-      sender: "ChatGPT"
+      message: "Hello, I'm PlayByGPT! Ask me anything!",
+      sentTime: "just now",
+      sender: "PlayByGPT"
     }
-  ])
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
 
+  // Handle sending a message
   const handleSend = async (message) => {
     const newMessage = {
-      message: message,
-      sender: "user",
-      direction: "outgoing"
-    }
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
 
     const newMessages = [...messages, newMessage];
 
     setMessages(newMessages);
 
-    setTyping(true);
+    // Show typing indicator and process the message with PlayByGPT
+    setIsTyping(true);
+    await processMessageToPlayByGPT(newMessages);
+  };
 
-    await processMessage(newMessages);
-  }
-
-
-  async function processMessage(chatMessages) {
-    
+  // Process the message with PlayByGPT using the OpenAI API
+  async function processMessageToPlayByGPT(chatMessages) {
+    // Format messages for PlayByGPT API
+    // Each message should be an object with 'role' (user or assistant) and 'content'
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
-      if(messageObject.sender === "ChatGPT") {
-        role = "assistant"
+      if (messageObject.sender === "PlayByGPT") {
+        role = "assistant";
       } else {
-        role = "user"
+        role = "user";
       }
-      return { role: role, content: messageObject.message}
+      return { role: role, content: messageObject.message }
     });
 
-    const systemMessage = {
-      role: "system",
-      content: "Explain all concepts like I am 10 years old."
-    }
-
+    // Create the API request body with the model and messages
+    // Add the system message at the beginning to define PlayByGPT's behavior
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
@@ -58,8 +64,8 @@ function App() {
       ]
     }
 
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
+    // Send the API request to OpenAI
+    await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Authorization": "Bearer " + API_KEY,
@@ -69,32 +75,30 @@ function App() {
     }).then((data) => {
       return data.json();
     }).then((data) => {
-      console.log(data);
-      console.log(data.choices[0].message.content);
-      setMessages(
-        [...chatMessages, {
-          message: data.choices[0].message.content,
-          sender: "ChatGPT"
-        }]
-      );
-      setTyping(false);
+      // Update the messages with the response from PlayByGPT
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "PlayByGPT"
+      }]);
+      setIsTyping(false);
     });
   }
 
   return (
     <div className="App">
-      <div style={{ position: "relative", height: "800px", width: "700px"}}>
+      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
         <MainContainer>
-          <ChatContainer>
-            <MessageList
-            scrollBehavior='smooth'
-            typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="PlayByGPT is typing" /> : null}
             >
               {messages.map((message, i) => {
+                console.log(message)
                 return <Message key={i} model={message} />
               })}
             </MessageList>
-            <MessageInput placeholder='Type message here' onSend={handleSend}/>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
           </ChatContainer>
         </MainContainer>
       </div>
